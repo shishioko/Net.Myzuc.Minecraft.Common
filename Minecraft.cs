c9ced1a373d4d6d96fdb9bfdcd278f1ad4c44226
shio <shishioko@proton.me>
2026-04-05 20:19:16 +0200

    added version info

---
+++ Minecraft.cs
@@ -0,0 +1,8 @@
+namespace Net.Myzuc.Minecraft.Common
+{
+    public static class Minecraft
+    {
+        public static Version Version { get; } = new(1, 21, 11, 0);
+        public static int ProtocolVersion { get; } = 774;
+    }
+}
