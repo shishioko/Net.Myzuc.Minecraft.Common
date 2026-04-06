d7d42266d728a66d99adad25249665af36f3723f
shio <shishioko@proton.me>
2026-04-06 22:49:40 +0200

    changed to nested class

--- Objects/GameProfile.cs
+++ Objects/GameProfile.cs
@@ -2,26 +2,25 @@ namespace Net.Myzuc.Minecraft.Common.Objects
 {
     public class GameProfile
     {
+        public struct Property
+        {
+            public string Name;
+            public string Value;
+            public string? Signature;
+            public Property(string name = "", string value = "", string? signature = null)
+            {
+                Name = name;
+                Value = value;
+                Signature = signature;
+            }
+        }
         public Guid Guid = Guid.Empty;
         public string Name = string.Empty;
-        public GameProfileProperty[] Properties = [];
+        public Property[] Properties = [];
 
         public GameProfile()
         {
             
-        }
-    }
-
-    public struct GameProfileProperty
-    {
-        public string Name;
-        public string Value;
-        public string? Signature;
-        public GameProfileProperty(string name = "", string value = "", string? signature = null)
-        {
-            Name = name;
-            Value = value;
-            Signature = signature;
         }
     }
 }
