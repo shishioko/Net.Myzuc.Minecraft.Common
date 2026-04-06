3012ced6226d187709028903e620684cb645f9b5
shio <shishioko@proton.me>
2026-04-06 22:46:35 +0200

    removed library redundant comments

--- Objects/GameProfile.cs
+++ Objects/GameProfile.cs
@@ -3,7 +3,7 @@ namespace Net.Myzuc.Minecraft.Common.Objects
     public class GameProfile
     {
         public Guid Guid = Guid.Empty;
-        public string Name = string.Empty; // 16 max len
+        public string Name = string.Empty;
 
         public GameProfileProperty[] Properties = [];
 
@@ -15,9 +15,9 @@ namespace Net.Myzuc.Minecraft.Common.Objects
 
     public class GameProfileProperty
     {
-        public string Name = string.Empty; // 64 max len
-        public string Value = string.Empty; // 32k max len
-        public string? Signature = null; // 1024 max len
+        public string Name = string.Empty;
+        public string Value = string.Empty;
+        public string? Signature = null;
 
         public GameProfileProperty()
         {
