a4fe2197578e50f8418a5242eb0fa0e465a465a6
Zffu <103074097+Zffu@users.noreply.github.com>
2026-04-06 19:05:20 +0200

    feat: added encryption response packet

---
+++ Protocol/Packets/Login/EncryptionResponsePacket.cs
@@ -0,0 +1,28 @@
+using Me.Shiokawaii.IO;
+
+namespace Net.Myzuc.Minecraft.Common.Protocol.Packets.Login;
+
+public sealed class EncryptionResponsePacket : Packet
+{
+    public override bool Serverbound => false;
+    public override ProtocolStage ProtocolStage => ProtocolStage.Login;
+    public override int Id => 0x01;
+
+    public byte[] shared_secret = new byte[] { };
+    public byte[] verify_token = new byte[] { };
+
+    public override void Serialize(Stream stream)
+    {
+        stream.WriteS32V(shared_secret.Length);
+        stream.WriteU8A(shared_secret);
+        
+        stream.WriteS32V(verify_token.Length);
+        stream.WriteU8A(verify_token);
+    }
+
+    public override void Deserialize(Stream stream)
+    {
+        shared_secret = stream.ReadU8A(stream.ReadS32V());
+        verify_token = stream.ReadU8A(stream.ReadS32V());
+    }
+}
