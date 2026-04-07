3cf3225b806b799e0cbaaa383afe1a577081897a
shishioko <shishioko@proton.me>
2026-04-07 10:55:29 +0200

    change visibility

--- Utilities/ServersideEncryptionUtility.cs
+++ Utilities/ServersideEncryptionUtility.cs
@@ -7,7 +7,7 @@ namespace Net.Myzuc.Minecraft.Common.Utilities
     {
         private readonly RSA Rsa;
         private readonly byte[] Sample;
-        private readonly string ServerId;
+        public string ServerId { get; private set; }
         public ServersideEncryptionUtility(string serverId = "", int sampleSize = 64)
         {
             Rsa = RSA.Create(1024);
