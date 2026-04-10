3568c23d4227a1ba2e8eb50d587cb4ab4c4692b3
shishioko <shishioko@proton.me>
2026-04-10 14:08:01 +0200

    seal

--- Objects/JsonConverters/GuidStringJsonConverter.cs
+++ Objects/JsonConverters/GuidStringJsonConverter.cs
@@ -3,7 +3,7 @@ using System.Text.Json.Serialization;
 
 namespace Net.Myzuc.Minecraft.Common.Objects.JsonConverters
 {
-    internal class GuidStringJsonConverter : JsonConverter<Guid>
+    internal sealed class GuidStringJsonConverter : JsonConverter<Guid>
     {
         public override Guid Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
