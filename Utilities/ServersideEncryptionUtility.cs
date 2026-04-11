d3bbc28b1005647dfb580d7fe8049b04ec1b2ed4
shishioko <shishioko@proton.me>
2026-04-12 01:10:51 +0200

    fix nullability

--- Utilities/ServersideEncryptionUtility.cs
+++ Utilities/ServersideEncryptionUtility.cs
@@ -61,8 +61,8 @@ namespace Net.Myzuc.Minecraft.Common.Utilities
             HttpResponseMessage auth = await http.GetAsync($"https://sessionserver.mojang.com/session/minecraft/hasJoined?username={Username}&serverId={hashstring}{(ip is not null ? $"&ip={ip}" : string.Empty)}");
             if (auth.StatusCode != HttpStatusCode.OK) throw new AuthenticationException();
             ResolvedProfile? profile = await JsonSerializer.DeserializeAsync<ResolvedProfile>(await auth.Content.ReadAsStreamAsync(), Global.JsonSerializerOptions);
-            if (!profile.HasValue) throw new NoNullAllowedException();
-            return profile.Value;
+            if (profile is null) throw new NoNullAllowedException();
+            return profile;
         }
     }
 }
